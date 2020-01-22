e364bd10cbb20eb650cd4c62096a430e16c063ae
awakescott <53873956+awakescott@users.noreply.github.com>
2020-01-22 17:50:54 -0500

    Code completed for movement and rotation
    The code has been completed for movement and rotation. At this point, the code just has to be tested to see if there are any errors in the code itself.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
 
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
     Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    public float turnSpeed = 20;
+    
 
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
 
@@ -29,5 +34,19 @@ public class PlayerMovement : MonoBehaviour
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
     }
+
+
+    void OnAnimatorMove()
+    {
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
 }
