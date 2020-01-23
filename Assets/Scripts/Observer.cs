5ee8d4a89ad4352d072866c00bf001272b75e864
Pynnacle <tpynn77@gmail.com>
2020-01-23 17:37:22 -0500

    Colliders + Observer script
    Added colliders to the gargoyle and vision object. Added basic functioning script for the gargoyle to detect the player. Vision is blocked by walls

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    //public GameEnding gameEnding;
+    bool isPlayerInRange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform == player)
+        {
+            isPlayerInRange = true;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            isPlayerInRange = false;
+            Debug.Log("Player lost");
+        }
+    }
+
+    void Update()
+    {
+        if(isPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if(raycastHit.collider.transform == player)
+                {
+                    Debug.Log("Player seen");
+                }
+            }
+        }
+    }
+}
