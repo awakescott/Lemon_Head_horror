9a6151d8bf770b97b2988dd0fbda25817aea6271
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2020-01-24 22:31:05 -0500

    Add C# script: ‘EndingGame’
    - Add C# script ‘EndingGame’ to the scripts folder.
    
    - Add starter code for necessary functionality: (float) fadeDuration, (GameObject) player, (void func) OnTriggerEnter

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f; //How long the screen takes to fade to black.
+    public GameObject player; //The game will only end if John Lemon himself enters the trigger collider.
+
+    void OnTriigerEnter(Collider other)
+    {
+        //To check that John Lemon is what/who collided with the trigger.
+        if(other.gameObject == player)
+        {
+
+        }
+    }
+}
