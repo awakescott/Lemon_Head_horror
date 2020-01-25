651007d61f82edd371844f8e446ace36360d65b8
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2020-01-24 22:51:01 -0500

    Add code to fade when John Lemon reaches exit
    Add 3 variables and 2 functions to ‘EndingGame’ script (see changes for details).

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -6,13 +6,33 @@ public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f; //How long the screen takes to fade to black.
     public GameObject player; //The game will only end if John Lemon himself enters the trigger collider.
+    public CanvasGroup exitBackgroundImageCanvasGroup; //Reference to the UI (Canvas)
+
+    bool m_IsPlayerAtExit; //This boolean is a way to know when to start fading the UI
+    float m_Timer; //A timer is needed to ensure that the game doesn't end before the fade is completed.
 
     void OnTriigerEnter(Collider other)
     {
         //To check that John Lemon is what/who collided with the trigger.
         if(other.gameObject == player)
         {
-
+            m_IsPlayerAtExit = true;
         }
+    }
+
+    //Checks if John Lemon is has reached the end of the level (by touching the trigger)
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    //Fades the screen then quits the game
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
     }
 }
